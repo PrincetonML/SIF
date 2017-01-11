a6ecfe7e06ea912f13fca1681f5a8b767074b81d
Yingyu Liang <lyycs42@gmail.com>
2017-01-11 11:08:05 -0500

    SIF embedding demo

---
+++ src/SIF_embedding.py
@@ -0,0 +1,57 @@
+import numpy as np
+from sklearn.decomposition import TruncatedSVD
+
+
+def get_weighted_average(We, x, w):
+    """
+    Compute the weighted average vectors
+    :param We: We[i,:] is the vector for word i
+    :param x: x[i, :] are the indices of the words in sentence i
+    :param w: w[i, :] are the weights for the words in sentence i
+    :return: emb[i, :] are the weighted average vector for sentence i
+    """
+    n_samples = x.shape[0]
+    emb = np.zeros((n_samples, We.shape[1]))
+    for i in xrange(n_samples):
+        emb[i,:] = w[i,:].dot(We[x[i,:],:]) / np.count_nonzero(w[i,:])
+    return emb
+
+def compute_pc(X,npc=1):
+    """
+    Compute the principal components
+    :param X: X[i,:] is a data point
+    :param npc: number of principal components to remove
+    :return: component_[i,:] is the i-th pc
+    """
+    svd = TruncatedSVD(n_components=npc, n_iter=7, random_state=0)
+    svd.fit(X)
+    return svd.components_
+
+def remove_pc(X, npc=1):
+    """
+    Remove the projection on the principal components
+    :param X: X[i,:] is a data point
+    :param npc: number of principal components to remove
+    :return: XX[i, :] is the data point after removing its projection
+    """
+    pc = compute_pc(X, npc)
+    if npc==1:
+        XX = X - X.dot(pc.transpose()) * pc
+    else:
+        XX = X - X.dot(pc.transpose()).dot(pc)
+    return XX
+
+
+def SIF_embedding(We, x, w, params):
+    """
+    Compute the scores between pairs of sentences using weighted average + removing the projection on the first principal component
+    :param We: We[i,:] is the vector for word i
+    :param x: x[i, :] are the indices of the words in the i-th sentence
+    :param w: w[i, :] are the weights for the words in the i-th sentence
+    :param params.rmpc: if >0, remove the projections of the sentence embeddings to their first principal component
+    :return: emb, emb[i, :] is the embedding for sentence i
+    """
+    emb = get_weighted_average(We, x, w)
+    if  params.rmpc > 0:
+        emb = remove_pc(emb, params.rmpc)
+    return emb
--- src/sim_algo.py
+++ src/sim_algo.py
@@ -1,46 +1,5 @@
 import numpy as np
-from sklearn.decomposition import TruncatedSVD
-
-
-def get_weighted_average(We, x, w):
-    """
-    Compute the weighted average vectors
-    :param We: We[i,:] is the vector for word i
-    :param x: x[i, :] are the indices of the words in sentence i
-    :param w: w[i, :] are the weights for the words in sentence i
-    :return: emb[i, :] are the weighted average vector for sentence i
-    """
-    n_samples = x.shape[0]
-    emb = np.zeros((n_samples, We.shape[1]))
-    for i in xrange(n_samples):
-        emb[i,:] = w[i,:].dot(We[x[i,:],:]) / np.count_nonzero(w[i,:])
-    return emb
-
-def compute_pc(X,npc=1):
-    """
-    Compute the principal components
-    :param X: X[i,:] is a data point
-    :param npc: number of principal components to remove
-    :return: component_[i,:] is the i-th pc
-    """
-    svd = TruncatedSVD(n_components=npc, n_iter=7, random_state=0)
-    svd.fit(X)
-    return svd.components_
-
-def remove_pc(X, npc=1):
-    """
-    Remove the projection on the principal components
-    :param X: X[i,:] is a data point
-    :param npc: number of principal components to remove
-    :return: XX[i, :] is the data point after removing its projection
-    """
-    pc = compute_pc(X, npc)
-    if npc==1:
-        XX = X - X.dot(pc.transpose()) * pc
-    else:
-        XX = X - X.dot(pc.transpose()).dot(pc)
-    return XX
-
+import SIF_embedding
 
 def weighted_average_sim_rmpc(We,x1,x2,w1,w2, params):
     """
@@ -53,11 +12,8 @@ def weighted_average_sim_rmpc(We,x1,x2,w1,w2, params):
     :param params.rmpc: if >0, remove the projections of the sentence embeddings to their first principal component
     :return: scores, scores[i] is the matching score of the pair i
     """
-    emb1 = get_weighted_average(We, x1, w1)
-    emb2 = get_weighted_average(We, x2, w2)
-    if  params.rmpc > 0:
-        emb1 = remove_pc(emb1, params.rmpc)
-        emb2 = remove_pc(emb2, params.rmpc)
+    emb1 = SIF_embedding.SIF_embedding(We, x1, w1, params)
+    emb2 = SIF_embedding.SIF_embedding(We, x2, w2, params)
 
     inn = (emb1 * emb2).sum(axis=1)
     emb1norm = np.sqrt((emb1 * emb1).sum(axis=1))
